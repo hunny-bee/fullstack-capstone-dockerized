ddb328d06de2a7d4f9d46228e8aefda502e0ef6a
hunny-bee <stringbonolo@gmail.com>
2024-09-13 04:32:39 +0200

    Created payment schema

---
+++ server/server/models/payment.js
@@ -0,0 +1,23 @@
+const PaymentSchema = new Schema({
+    booking: {
+      type: Schema.Types.ObjectId,
+      ref: 'Booking',
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ['credit_card', 'paypal', 'crypto'],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['completed', 'failed'],
+      default: 'completed',
+    },
+  }, { timestamps: true });
+  
+  module.exports = mongoose.model('Payment', PaymentSchema);
