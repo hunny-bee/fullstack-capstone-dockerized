f462c5a2cea6d17d0415f6295d306ae6660a5185
hunny-bee <stringbonolo@gmail.com>
2024-09-20 12:23:25 +0200

    removed comments

--- server/server/routes/paymentRoutes.js
+++ server/server/routes/paymentRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const paymentController = require("../controllers/paymentController")
+const paymentController = require("../controllers/paymentController");
 
-//process payment
+
 router.post("/process", paymentController.processPayment);
 
-//get payment details by Id
 router.get("/:id", paymentController.getPaymentById);
 
 module.exports = router;
