534907fe9a45654c9a5d63b8387e0603b8591a90
Lethukuthula Mkhonto <125813299+LethuM2197@users.noreply.github.com>
2024-09-25 14:19:30 +0200

    Updated property routes

---
+++ server/server/routes/propertyRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const propertyController = require('../controllers/propertyController');
+const { authenticateUser, hostOnly } = require('../middleware/authMiddleware');
+
+
+router.post('/', authenticateUser, hostOnly, propertyController.createProperty); 
+router.get('/', propertyController.getAllProperties);
+router.get('/:id', propertyController.getPropertyById);
+router.put('/:id', authenticateUser, hostOnly, propertyController.updateProperty);
+router.delete('/:id',authenticateUser, hostOnly, propertyController.deleteProperty); 
+router.get('/host/:hostId', propertyController.getPropertiesByHost); 
+
+module.exports = router;
